f2d79453e5c0aec49a1f1ca399952341f782c0c0
Ashley Zhang <ayz27@cornell.edu>
2024-12-09 15:59:05 -0500

    add serial skeleton

--- src/serial.cpp
+++ src/serial.cpp
@@ -1,10 +1,20 @@
 #include "common_data_types.h"
 
+// serial implementation of search
+std::string flight_finder::search() {
+    return itinerary().serialize(flights);
+}
+
 int main(int argc, char** argv) {
     flight_constraints constrs = cli("serial", argc, argv);
-    (void)constrs;
 
-    std::cout << "serial" << std::endl;
+    std::cout << "running serial" << std::endl;
+
+    // TODO: call parser
+    std::vector<flight> temp;
+
+    flight_finder ff(std::move(temp), constrs.origin);
+    std::cout << ff.search() << std::endl;
     
     return 0;
 }
