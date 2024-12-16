0d9b1e40296d770e6ee3cf29f0afed345c96e9ca
Ashley Zhang <ayz27@cornell.edu>
2024-12-16 11:31:26 -0500

    add parallel skeleton

--- src/parallel.cpp
+++ src/parallel.cpp
@@ -1,4 +1,5 @@
 #include "common_data_types.h"
+#include "parser.h"
 
 // parallel implementation of search
 template<>
@@ -10,9 +11,14 @@ std::string flight_finder::search<OptLevel::PARALLEL>() {
 #ifndef REMOVE_MAIN_FUNC
 int main(int argc, char** argv) {
     flight_constraints constrs = cli("parallel", argc, argv);
-    (void)constrs;
 
     std::cout << "parallel" << std::endl;
+
+    std::string directory = "flight_concurr_arr_results";
+    std::vector<flight> flights = parse_flights_from_directory(directory, constrs);
+
+    flight_finder ff(std::move(flights), constrs.origin);
+    std::cout << ff.search<OptLevel::PARALLEL>() << std::endl;
     
     return 0;
 }
